c19afbb5f38cecc2752b947462e7a116454a7051
GabrielleLimaNogueira <gabriellevitorialimasenai@gmail.com>
2026-04-16 15:53:53 -0300

    introdução
    introdução javascript

---
+++ aula1-introdução/scipts/aula.js
@@ -0,0 +1,33 @@
+//resposta no  console
+console.log("Olá mundo");
+//caixas de diaçogo
+//alert("FRONT É MATERIA MAIS TOP");
+//prompt("Qual o seu nome?");
+//confirm("Você faria um pix para seu colega?");
+
+//acessa documento, busca o elemento com o id selecionado
+//inser =dentro
+document.getElementById("resposta").innerText ="<h1>QUE LEGAL<h1>"
+document.getElementById("resposta").innerHTML ="<h1>QUE LEGAL<h1>"
+
+//string -caractere ("Gabrielle")
+var nome = "Gabrielle";
+
+//boolean -lógico ( 'verdadeiro' - true 'falso' - false)
+var ligado = true;
+//number - numero(80)
+
+//*obs para numeros quebrados- float()
+//*obs para numero inteiros - int()
+var idade = 16;
+
+var dinheiro = 108.50
+
+//arry - lista , vetor
+
+var listaCompras
+console.log(typeof(nome));
+console.log(typeof(ligado));
+console.log(typeof(idade));
+console.log(typeof(dinheiro));
+console.log(typeof(listaCompras));
---
+++ aula1-introdução/scipts/usuario.js
@@ -0,0 +1,3 @@
+var  nome = prompt("Qual o seu nome? ")
+//document.getElementById("resposta") innerHTML = <h1> +nome+<h1>
+document.getElementById("resposta").innerHTML = `<h1>olá seja bem vindo(a) ${nome}<h1>`;
